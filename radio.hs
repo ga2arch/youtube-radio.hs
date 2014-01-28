72ce0febb4b86e728b902358dccf3a557b6cb36d
Gabriele <ga2arch@MacBook-Pro-di-Gabriele.local>
2014-01-28 21:51:28 +0100

    add streamUrl and fixed lame inputs

--- radio.hs
+++ radio.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Conduit.Process.Unix (forkExecuteFile)
-
+import Control.Monad
+import Data.Conduit.Process.Unix (forkExecuteFile, waitForProcess)
+import System.Process (readProcess)
 import qualified Data.ByteString.UTF8 as BU
+
+--------------------------------------
 
 mpv url = do
   p <- forkExecuteFile
@@ -20,13 +23,27 @@ mpv url = do
          Nothing
   return p
 
-lame mpvStdout radioOut = do
+lame = do
   p <- forkExecuteFile
           "lame"
-          ["--quiet", "/tmp/pipe.mp3", "/tmp/radio.mp3"]
+          ["--quiet",
+           "/tmp/pipe.mp3",
+           "/tmp/radio.mp3"]
           Nothing
           Nothing
           Nothing
           Nothing
           Nothing
   return p
+
+--------------------------------------
+
+streamUrl yurl = do
+  tmp <- readProcess "youtube-dl" ["-g", yurl] []
+  let url = take (length tmp - 1) tmp
+
+  when (url !! 4 /= 's') $ do
+    mpv url
+    l <- lame
+    _ <- waitForProcess l
+    return()
