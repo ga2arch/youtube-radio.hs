6b51525daca438520a9cb10e2d776edbc283fa08
Gabriele <ga2arch@MacBook-Pro-di-Gabriele.local>
2014-01-28 21:46:58 +0100

    Added mpv method

--- radio.hs
+++ radio.hs
@@ -1,1 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Conduit.Process.Unix (forkExecuteFile)
+
+import qualified Data.ByteString.UTF8 as BU
+
+mpv url = do
+  mpv <- forkExecuteFile
+         "mpv"
+         ["--no-video",
+          "-ao",
+          "pcm:file=/tmp/pipe.mp3",
+          "--really-quiet",
+         (BU.fromString url)]
+         Nothing
+         Nothing
+         Nothing
+         Nothing
+         Nothing
+  return mpv
