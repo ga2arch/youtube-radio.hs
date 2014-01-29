8c70a105e3725a5acdb5a383e651ba9a29bebc23
Gabriele <ga2arch@MacBook-Pro-di-Gabriele.local>
2014-01-29 15:50:07 +0100

    fuck the radio, trasconding single files

--- radio.hs
+++ radio.hs
@@ -5,91 +5,84 @@ import Control.Monad
 import Control.Monad.STM
 import Control.Concurrent
 import Data.Conduit
-import Data.Conduit.Process.Unix (forkExecuteFile, waitForProcess)
+import Data.Conduit.Process.Unix
 import Data.Conduit.TMChan
+import GHC.IO.Handle.FD
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.HTTP.Types (status200)
-import System.IO (readFile)
+import System.IO
 import System.Directory
 import System.Process (readProcess)
 import System.Random (randomRIO)
 import System.Posix.Files
 
 import qualified Blaze.ByteString.Builder.ByteString as BBB
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as CL
 import qualified Data.ByteString.UTF8 as BU
 
 --------------------------------------
 
-mpv url = do
+mpv url stdout = do
   p <- forkExecuteFile
          "mpv"
          ["--no-video",
           "-ao",
-          "pcm:file=/tmp/pipe.mp3",
+          "pcm:file=/dev/stdout",
           "--really-quiet",
-         (BU.fromString url)]
+          (BU.fromString url)]
          Nothing
          Nothing
          Nothing
-         Nothing
+         (Just $ sinkTBMChan stdout False)
          Nothing
   return p
 
-lame = do
+lame mpvStdout stdout = do
   p <- forkExecuteFile
           "lame"
           ["--quiet",
-           "/tmp/pipe.mp3",
-           "/tmp/radio.mp3"]
+           "-",
+           "-"]
           Nothing
           Nothing
-          Nothing
-          Nothing
+          (Just $ sourceTBMChan mpvStdout)
+          (Just $ sinkTBMChan stdout False)
           Nothing
   return p
 
 --------------------------------------
 
-cleanUp = do
-  removeFile "/tmp/pipe.mp3"
-  createNamedPipe "/tmp/pipe.mp3" namedPipeMode
-
 streamUrl yurl = do
-  tmp <- readProcess "youtube-dl" ["-g", yurl] []
-  let url = take (length tmp - 1) tmp
+  --tmp <- readProcess "youtube-dl" ["-g", (BU.toString yurl)] []
+  --let url = take (length tmp - 1) tmp
 
-  when (url !! 4 /= 's') $ do
-    mpv url
-    l <- lame
-    _ <- waitForProcess l
-    return()
+  putStrLn (BU.toString yurl)
 
-queue = do
-  yurls <- fmap lines (readFile "playlist")
-  yurl <- pick yurls
-  streamUrl yurl
-  cleanUp
-  queue
-  where
-    pick xs = randomRIO (0, (length xs - 1)) >>= return . (xs !!)
+  mpvOut <- atomically $ newTBMChan 16
+  lameOut <- atomically $ newTBMChan 16
 
-radio c = do
-  runResourceT $
-    CB.sourceFile "/tmp/radio.mp3"
-    $= CL.map (Chunk . BBB.fromByteString)
-    $$ sinkTMChan c False
+--when (url !! 4 /= 's') $ do
+  m <- mpv (BU.toString yurl) mpvOut
+  l <- lame mpvOut lameOut
+  return (m, l, lameOut)
 
-app input _ = do
-  chan <- atomically $ dupTMChan input
-  return $ responseSource status200 [] $ sourceTMChan chan
+  --return ()
+
+app req = do
+  let query = queryString req
+  let (Just (Just yurl)) = lookup "yurl" query
+
+  responseSourceBracket
+    (streamUrl yurl)
+    (\(m, l, _) -> terminateProcess l >> terminateProcess m)
+    (\(_, _, lameOut) -> do
+      let source = sourceTBMChan lameOut
+                   $= CL.map (Chunk . BBB.fromByteString)
+      return (status200, [], source))
 
 main = do
-  r <- atomically $ newBroadcastTMChan
-  forkIO queue
-  forkIO $ radio r
-
-  _ <- run 3000 (app r)
+  _ <- run 3000 app
   return ()
